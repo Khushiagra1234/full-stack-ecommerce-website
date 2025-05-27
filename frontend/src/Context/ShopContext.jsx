32e71d6cbbfa476380e97399c98bf75363354813
Khushi Tayal <khushiagra1111@gmail.com>
2025-05-28 03:02:38 +0530

    Your commit message

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,5 @@
 
-import React,{ createContext, useEffect, useState } from 'react';
+/*import React,{ createContext, useEffect, useState } from 'react';
 
 export const ShopContext = createContext(null);
 
@@ -82,6 +82,112 @@ const ShopContextProvider = (props) => {
 .then((data) => console.log('Removed from cart:', data))
 .catch((error) => console.error('Remove from cart failed:', error));
  
+        }
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+            return totalAmount;
+    }
+    
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for(const item in cartItems){
+            if( cartItems[item] > 0)
+            {
+               totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount, all_product,cartItems,addToCart,removeFromCart};
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider
+*/
+
+import React, { createContext, useEffect, useState } from 'react';
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = ()=>{
+    let cart = {};
+    for(let index = 0; index < 300+1; index++){
+        cart[index] = 0;
+    }
+    return cart;
+}
+
+const ShopContextProvider = (props) => {
+
+    const [all_product,setAll_Product] = useState([]);
+    const [cartItems,setCartItems] = useState(getDefaultCart());
+    
+    useEffect(()=>{
+        fetch('https://full-stack-ecommerce-website-2-1z1t.onrender.com/allproducts')
+        .then((response)=>response.json())
+        .then((data)=>setAll_Product(data))
+
+        // auth token is available loged in and start data will be fetch and save it in Shop context
+        if(localStorage.getItem('auth-token')){
+            fetch('https://full-stack-ecommerce-website-2-1z1t.onrender.com/getcart',{
+                method:'POST',
+                headers:{
+                    Accept:'appilcation/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:"",
+            }).then((response)=>response.json())
+            .then((data)=>setCartItems(data))
+        }
+    },[])
+    
+    
+
+    const addToCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        if(localStorage.getItem('auth-token')){
+            fetch('https://full-stack-ecommerce-website-2-1z1t.onrender.com/addtocart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data));
+        }
+   }
+
+    const removeFromCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        if(localStorage.getItem('auth-token')){
+            fetch('https://full-stack-ecommerce-website-2-1z1t.onrender.com/removefromcart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data)); 
         }
     }
 
