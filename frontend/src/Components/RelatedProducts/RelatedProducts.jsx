ea5cefa4d66cbbd99db09f4d730cc8ecd92fd18d
Khushi <146318133+Khushiagra1234@users.noreply.github.com>
2025-06-21 00:59:26 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import './RelatedProducts.css'
-import data_product from '../Assets/data'
 import Item from '../Items/Item'
 
 
+
+
 const RelatedProducts = () => {
+  
+  const [relatedProducts,setRelatedProducts] = useState([]);
+  
+    useEffect(()=>{
+      fetch('https://full-stack-ecommerce-website-5.onrender.com/popularinwomen'
+  )
+      .then((response)=>response.json())
+      .then((data)=>setRelatedProducts(data));
+    },[])
   return (
     <div className='relatedproducts'>
         <h1>Related Products</h1>
         <hr />
         <div className="relatedproducts-item">
-        {data_product.map((item,i)=>{
+        {relatedProducts.map((item,i)=>{
         return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
     })}
         </div>
